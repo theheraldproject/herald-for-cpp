f2d5d3905b86e5866c10757cc650dd5dd7b5c530
Adam Fowler <adamfowleruk@gmail.com>
2021-03-17 09:14:55 +0000

    VariantSet and ListManager working reliably
    No copy construction, one instance per variant type
    Signed-off-by: Adam Fowler <adamfowleruk@gmail.com>

--- herald-tests/analysisrunner-tests.cpp
+++ herald-tests/analysisrunner-tests.cpp
@@ -9,70 +9,135 @@
 using namespace herald::analysis::sampling;
 using namespace herald::datatype;
 
-template <std::size_t Sz>
-struct DummyRSSISource {
-  using value_type = Sample<RSSI>; // allows AnalysisRunner to introspect this class at compile time
+// template <std::size_t Sz>
+// struct DummyRSSISource {
+//   using value_type = Sample<RSSI>; // allows AnalysisRunner to introspect this class at compile time
 
-  DummyRSSISource(const std::size_t srcDeviceKey, SampleList<Sample<RSSI>,Sz>&& data) : key(srcDeviceKey), data(std::move(data)) {};
-  ~DummyRSSISource() = default;
+//   DummyRSSISource(const std::size_t srcDeviceKey, SampleList<Sample<RSSI>,Sz>&& data) : key(srcDeviceKey), data(std::move(data)) {};
+//   ~DummyRSSISource() = default;
 
-  template <typename RunnerT>
-  void run(int timeTo, RunnerT& runner) {
-    // get reference to target list
-    // auto& devList = runner.list<RSSI>(key);
-    // push through data at default rate
-    for (auto& v: data) {
-      // devList.push(v.taken,v.value); // copy data over (It's unusual taking a SampleList and sending to a SampleList)
-      if (v.taken.secondsSinceUnixEpoch() <= timeTo) {
-        runner.template newSample<RSSI,20>(key,v);
-      }
-    }
-    runner.run(timeTo);
+//   template <typename RunnerT>
+//   void run(int timeTo, RunnerT& runner) {
+//     // get reference to target list
+//     // auto& devList = runner.list<RSSI>(key);
+//     // push through data at default rate
+//     for (auto& v: data) {
+//       // devList.push(v.taken,v.value); // copy data over (It's unusual taking a SampleList and sending to a SampleList)
+//       if (v.taken.secondsSinceUnixEpoch() <= timeTo) {
+//         runner.template newSample<RSSI,20>(key,v);
+//       }
+//     }
+//     runner.run(timeTo);
+//   }
+
+// private:
+//   std::size_t key;
+//   SampleList<Sample<RSSI>,Sz> data;
+// };
+
+// struct DummyDistanceDelegate : public herald::analysis::AnalysisDelegate {
+//   DummyDistanceDelegate() : AnalysisDelegate() {};
+//   ~DummyDistanceDelegate() {};
+
+//   // Detected methods by AnalysisRunner
+//   void newSample(SampledID sampled, Sample<Distance> sample) {
+//     lastSampledID = sampled;
+//     distances.push(sample);
+//   }
+
+//   template <typename ValT>
+//   void newSample(SampledID sampled, Sample<ValT> sample) {
+//     ; // general handler - specialisation above handles Distance samples
+//   }
+
+//   template <typename RunnerT>
+//   void setDestination(RunnerT& runner) {
+//     // TODO in real life you'd care about this
+//   }
+
+//   void reset() {
+//     distances.clear();
+//     lastSampledID = 0;
+//   }
+
+//   // Test only methods
+//   SampledID lastSampled() {
+//     return lastSampledID;
+//   }
+
+//   const SampleList<Sample<Distance>,20>& samples() {
+//     return distances;
+//   }
+
+// private:
+//   SampledID lastSampledID;
+//   SampleList<Sample<Distance>,20> distances;
+// };
+
+
+TEST_CASE("variantset-basic", "[variantset][basic]") {
+  SECTION("variantset-basic") {
+    herald::analysis::VariantSet<int,double> vs;
+    REQUIRE(vs.size() == 2);
+    int intValue = vs.get<int>();
+    REQUIRE(intValue == 0);
+    double dblValue = vs.get<double>();
+    REQUIRE(dblValue == 0.0);
   }
+}
 
-private:
-  std::size_t key;
-  SampleList<Sample<RSSI>,Sz> data;
-};
+TEST_CASE("variantset-lists", "[variantset][lists]") {
+  SECTION("variantset-lists") {
+    herald::analysis::VariantSet<SampleList<Sample<int>,15>,SampleList<Sample<double>,15>> vs;
+    REQUIRE(vs.size() == 2);
+    SampleList<Sample<int>,15>& intValue = vs.get<SampleList<Sample<int>,15>>();
+    REQUIRE(intValue.size() == 0);
+    SampleList<Sample<double>,15>& dblValue = vs.get<SampleList<Sample<double>,15>>();
+    REQUIRE(dblValue.size() == 0);
 
-struct DummyDistanceDelegate : public herald::analysis::AnalysisDelegate {
-  DummyDistanceDelegate() : AnalysisDelegate() {};
-  ~DummyDistanceDelegate() {};
+    intValue.push(0,12);
+    dblValue.push(10,14.3);
+    dblValue.push(20,15.3);
 
-  // Detected methods by AnalysisRunner
-  void newSample(SampledID sampled, Sample<Distance> sample) {
-    lastSampledID = sampled;
-    distances.push(sample);
+    SampleList<Sample<int>,15>& intValue2 = vs.get<SampleList<Sample<int>,15>>();
+    REQUIRE(intValue2.size() == 1);
+    SampleList<Sample<double>,15>& dblValue2 = vs.get<SampleList<Sample<double>,15>>();
+    REQUIRE(dblValue2.size() == 2);
+
   }
+}
 
-  template <typename ValT>
-  void newSample(SampledID sampled, Sample<ValT> sample) {
-    ; // general handler - specialisation above handles Distance samples
+TEST_CASE("variantset-listmanager", "[variantset][listmanager]") {
+  SECTION("variantset-listmanager") {
+    using herald::analysis::ListManager;
+    herald::analysis::VariantSet<ListManager<int,15,50>,ListManager<double,15,50>> vs;
+    REQUIRE(vs.size() == 2);
+    ListManager<int,15,50>& intValue = vs.template get<ListManager<int,15,50>>();
+    REQUIRE(intValue.size() == 0);
+    ListManager<double,15,50>& dblValue = vs.template get<ListManager<double,15,50>>();
+    REQUIRE(dblValue.size() == 0);
+    
+    SampleList<Sample<int>,15>& intList = intValue.list(1234); // list for entity being sampled with SampledId=1234
+    REQUIRE(intList.size() == 0);
+    SampleList<Sample<double>,15>& dblList = dblValue.list(5678); // list for entity being sampled with SampledId=1234
+    REQUIRE(dblList.size() == 0);
+
+    intList.push(Sample(0,12));
+    dblList.push(Sample(10,14.3));
+    dblList.push(Sample(20,15.3));
+
+    ListManager<int,15,50>& intValue2 = vs.template get<ListManager<int,15,50>>();
+    REQUIRE(intValue2.size() == 1);
+    ListManager<double,15,50>& dblValue2 = vs.template get<ListManager<double,15,50>>();
+    REQUIRE(dblValue2.size() == 1);
+    
+    SampleList<Sample<int>,15>& intList2 = intValue2.list(1234); // list for entity being sampled with SampledId=1234
+    REQUIRE(intList2.size() == 1);
+    SampleList<Sample<double>,15>& dblList2 = dblValue2.list(5678); // list for entity being sampled with SampledId=1234
+    REQUIRE(dblList2.size() == 2);
+
   }
-
-  template <typename RunnerT>
-  void setDestination(RunnerT& runner) {
-    // TODO in real life you'd care about this
-  }
-
-  void reset() {
-    distances.clear();
-    lastSampledID = 0;
-  }
-
-  // Test only methods
-  SampledID lastSampled() {
-    return lastSampledID;
-  }
-
-  const SampleList<Sample<Distance>,20>& samples() {
-    return distances;
-  }
-
-private:
-  SampledID lastSampledID;
-  SampleList<Sample<Distance>,20> distances;
-};
+}
 
 /// [Who]   As a DCT app developer
 /// [What]  I want to link my live application data to an analysis runner easily
@@ -81,36 +146,36 @@ private:
 /// [Who]   As a DCT app developer
 /// [What]  I want to periodically run analysis aggregates automatically
 /// [Value] So I don't miss any information, and have accurate, regular, samples
-TEST_CASE("analysisrunner-basic", "[analysisrunner][basic]") {
-  SECTION("analysisrunner-basic") {
-    SampleList<Sample<RSSI>,10> srcData{
-      Sample<RSSI>{10,-55},Sample<RSSI>{20,-55},Sample<RSSI>{30,-55},Sample<RSSI>{40,-55},Sample<RSSI>{50,-55},
-      Sample<RSSI>{60,-55},Sample<RSSI>{70,-55},Sample<RSSI>{80,-55},Sample<RSSI>{90,-55},Sample<RSSI>{100,-55}
-    };
-    DummyRSSISource src(1234,std::move(srcData));
+// TEST_CASE("analysisrunner-basic", "[analysisrunner][basic]") {
+//   SECTION("analysisrunner-basic") {
+//     SampleList<Sample<RSSI>,10> srcData{
+//       Sample<RSSI>{10,-55},Sample<RSSI>{20,-55},Sample<RSSI>{30,-55},Sample<RSSI>{40,-55},Sample<RSSI>{50,-55},
+//       Sample<RSSI>{60,-55},Sample<RSSI>{70,-55},Sample<RSSI>{80,-55},Sample<RSSI>{90,-55},Sample<RSSI>{100,-55}
+//     };
+//     DummyRSSISource src(1234,std::move(srcData));
 
-    herald::analysis::algorithms::distance::FowlerBasicAnalyser distanceAnalyser(30, -50, -24);
+//     herald::analysis::algorithms::distance::FowlerBasicAnalyser distanceAnalyser(30, -50, -24);
 
-    herald::analysis::AnalysisRunner<RSSI,Distance> runner; // just for Sample<RSSI> types, and their produced output (Sample<Distance>)
+//     herald::analysis::AnalysisRunner<RSSI,Distance> runner; // just for Sample<RSSI> types, and their produced output (Sample<Distance>)
 
-    std::shared_ptr<DummyDistanceDelegate> myDelegate = std::make_shared<DummyDistanceDelegate>();
-    runner.addAnalyser(distanceAnalyser); // so it picks up new data items
-    runner.add(myDelegate); // so we receive the relevant output
+//     std::shared_ptr<DummyDistanceDelegate> myDelegate = std::make_shared<DummyDistanceDelegate>();
+//     runner.addAnalyser(distanceAnalyser); // so it picks up new data items
+//     runner.add(myDelegate); // so we receive the relevant output
 
-    // run at different times and ensure that it only actually runs three times (sample size == 3)
-    src.run(20,runner);
-    src.run(40,runner);
-    src.run(60,runner);
-    src.run(80,runner);
-    src.run(95,runner);
+//     // run at different times and ensure that it only actually runs three times (sample size == 3)
+//     src.run(20,runner);
+//     src.run(40,runner);
+//     src.run(60,runner);
+//     src.run(80,runner);
+//     src.run(95,runner);
 
-    auto& samples = myDelegate->samples();
-    REQUIRE(samples.size() == 3); // didn't reach 4x30 seconds, so no tenth sample
-    REQUIRE(samples[0].taken.secondsSinceUnixEpoch() == 30);
-    REQUIRE(samples[0].value != 0.0);
-    REQUIRE(samples[1].taken.secondsSinceUnixEpoch() == 60);
-    REQUIRE(samples[1].value != 0.0);
-    REQUIRE(samples[2].taken.secondsSinceUnixEpoch() == 90);
-    REQUIRE(samples[2].value != 0.0);
-  }
-}
+//     auto& samples = myDelegate->samples();
+//     REQUIRE(samples.size() == 3); // didn't reach 4x30 seconds, so no tenth sample
+//     REQUIRE(samples[0].taken.secondsSinceUnixEpoch() == 30);
+//     REQUIRE(samples[0].value != 0.0);
+//     REQUIRE(samples[1].taken.secondsSinceUnixEpoch() == 60);
+//     REQUIRE(samples[1].value != 0.0);
+//     REQUIRE(samples[2].taken.secondsSinceUnixEpoch() == 90);
+//     REQUIRE(samples[2].value != 0.0);
+//   }
+// }
--- herald/include/herald/analysis/runner.h
+++ herald/include/herald/analysis/runner.h
@@ -28,24 +28,83 @@ struct AnalysisDelegate {
 };
 
 /// \brief Manages a set of lists for a particular Sample Value Type
-template <typename ValT, std::size_t Size>
+template <typename ValT, std::size_t Size, std::size_t MaxLists>
 struct ListManager {
   using value_type = ValT;
 
-  ListManager() = default;
+  ListManager() noexcept : lists(), nextPos(0) {};
+  ListManager(ListManager&& other) : lists(std::move(other.lists)), nextPos(other.nextPos) {}; // move ctor
   ~ListManager() = default;
 
-  decltype(auto) list(const SampledID sampled) {
-    return lists.try_emplace(lists.end(),sampled,{}).first;
+  SampleList<Sample<ValT>,Size>& list(const SampledID sampled) {
+    for (auto& entry : lists) {
+      if (entry.key() == sampled) {
+        return entry.value();
+      }
+    }
+    auto& val = lists[nextPos++];
+    return val.emplace(sampled);
   }
 
   void remove(const SampledID listFor) {
-    lists.erase(listFor);
+    // lists.erase(listFor);
+    // TODO delete value and re-use its allocation space
+  }
+
+  const std::size_t size() const {
+    return nextPos;
   }
 
 private:
-  std::map<SampledID,SampleList<Sample<ValT>,Size>> lists;
+  template <typename Key>
+  struct ListManagerEntry {
+    ListManagerEntry() : mKey(), mValue() {}
+    ~ListManagerEntry() = default;
+
+    SampleList<Sample<ValT>,Size>& emplace(const Key k) {
+      mKey = k;
+      return value();
+    }
+
+    const Key key() const {
+      return mKey;
+    }
+
+    SampleList<Sample<ValT>,Size>& value() {
+      return mValue;
+    }
+
+  private:
+    Key mKey;
+    SampleList<Sample<ValT>,Size> mValue;
+  };
+
+  std::array<ListManagerEntry<SampledID>,MaxLists> lists;
+  int nextPos;
 };
+// template <typename ValT, std::size_t Size>
+// struct ListManager {
+//   using value_type = ValT;
+
+//   ListManager() = default;
+//   ~ListManager() = default;
+
+//   SampleList<Sample<ValT>,Size>& list(const SampledID sampled) {
+//     auto iter = lists.try_emplace(sampled).first;
+//     return lists.at(sampled);
+//   }
+
+//   void remove(const SampledID listFor) {
+//     lists.erase(listFor);
+//   }
+
+//   const std::size_t size() const {
+//     return lists.size();
+//   }
+
+// private:
+//   std::map<SampledID,SampleList<Sample<ValT>,Size>> lists;
+// };
 
 /// \brief A fixed size set that holds exactly one instance of the std::variant for each
 /// of the specified ValTs value types.
@@ -53,7 +112,9 @@ template <typename... ValTs>
 struct VariantSet {
   static constexpr std::size_t Size = sizeof...(ValTs);
 
-  VariantSet() : variants({ValTs()...}) {}; // Instantiate each type instance in the array
+  VariantSet() : variants() {
+    createInstances<ValTs...>(0);
+  }; // Instantiate each type instance in the array
   ~VariantSet() = default;
 
   /// CAN THROW std::bad_variant_access
@@ -67,8 +128,22 @@ struct VariantSet {
     throw std::bad_variant_access();
   }
 
+  const std::size_t size() const {
+    return variants.size();
+  }
+
 private:
   std::array<std::variant<ValTs...>,Size> variants;
+  template <typename LastT>
+  void createInstances(int pos) {
+    variants[pos].template emplace<LastT>();
+  }
+
+  template <typename FirstT, typename SecondT, typename... RestT>
+  void createInstances(int pos) {
+    variants[pos].template emplace<FirstT>();
+    createInstances<SecondT,RestT...>(pos + 1);
+  }
 };
 
 /// The below is an example ValueSource...
@@ -90,68 +165,68 @@ private:
 /// This class can be used 'live' against real sensors, or statically with reference data. 
 /// This is achieved by ensuring the run(Date) method takes in the Date for the time of evaluation rather
 /// than using the current Date.
-template <typename... SourceTypes>
-struct AnalysisRunner {
-  // using valueTypes = (typename SourceTypes::value_type)...;
+// template <typename... SourceTypes>
+// struct AnalysisRunner {
+//   // using valueTypes = (typename SourceTypes::value_type)...;
 
-  AnalysisRunner(/*SourceTypes&... sources*/) : lists(), notifiables() {
-    // registerRunner(sources...);
-  }
-  ~AnalysisRunner() = default;
+//   AnalysisRunner(/*SourceTypes&... sources*/) : lists(), notifiables() {
+//     // registerRunner(sources...);
+//   }
+//   ~AnalysisRunner() = default;
 
-  /// We are an analysis delegate ourselves - this is used by Source types, and by producers (analysis runners)
-  template <typename ValT, std::size_t Size>
-  void newSample(SampledID sampled, sampling::Sample<ValT> sample) {
-    // incoming sample. Pass to correct list
-    lists.template get<ListManager<ValT,Size>>().list(sampled).second.push(sample);
-  }
+//   /// We are an analysis delegate ourselves - this is used by Source types, and by producers (analysis runners)
+//   template <typename ValT, std::size_t Size>
+//   void newSample(SampledID sampled, sampling::Sample<ValT> sample) {
+//     // incoming sample. Pass to correct list
+//     lists.template get<ListManager<ValT,Size>>().list(sampled)->second.push(sample);
+//   }
 
-  /// Run the relevant analyses given the current time point
-  void run(Date timeNow) {
-    // call analyse(dateNow,srcList,dstDelegate) for all delegates with the correct list each, for each sampled
-    // TODO performance enhancement - 'dirty' sample lists only (ones with new data)
-    for (auto& listManager : lists) {
-      using ValT = listManager::value_type;
-      for (auto& delegate : notifiables) {
-        // if constexpr (std::is_same_v<ValT,delegate::value_type>) { // SHOULD BE RUNNERS NOT DELEGATES
-          //delegate.
-        // }
-      }
-    }
-  }
+//   /// Run the relevant analyses given the current time point
+//   void run(Date timeNow) {
+//     // call analyse(dateNow,srcList,dstDelegate) for all delegates with the correct list each, for each sampled
+//     // TODO performance enhancement - 'dirty' sample lists only (ones with new data)
+//     for (auto& listManager : lists) {
+//       using ValT = listManager::value_type;
+//       for (auto& delegate : notifiables) {
+//         // if constexpr (std::is_same_v<ValT,delegate::value_type>) { // SHOULD BE RUNNERS NOT DELEGATES
+//           //delegate.
+//         // }
+//       }
+//     }
+//   }
 
-  void add(std::shared_ptr<AnalysisDelegate> delegate) {
-    notifiables.push_back(delegate);
-  }
+//   void add(std::shared_ptr<AnalysisDelegate> delegate) {
+//     notifiables.push_back(delegate);
+//   }
 
-  template <typename AnalyserT>
-  void addAnalyser(AnalyserT& analyser) {
-    // TODO fill this out and call its analyse() function when required
-  }
+//   template <typename AnalyserT>
+//   void addAnalyser(AnalyserT& analyser) {
+//     // TODO fill this out and call its analyse() function when required
+//   }
 
-  // /// callback from analysis data source
-  // template <typename ValT>
-  // SampleList<Sample<ValT>>& list(const SampledID sampled) {
-  //   return lists.get<ValT>().list(sampled);
-  // }
+//   // /// callback from analysis data source
+//   // template <typename ValT>
+//   // SampleList<Sample<ValT>>& list(const SampledID sampled) {
+//   //   return lists.get<ValT>().list(sampled);
+//   // }
 
-private:
-  // TODO make sizes a parameterised list derived from template parameters
-  VariantSet<ListManager<SourceTypes,25>...> lists; // exactly one list manager per value type
-  std::vector<std::shared_ptr<AnalysisDelegate>> notifiables; // more than one delegate per value type
-  //std::vector< // runners
+// private:
+//   // TODO make sizes a parameterised list derived from template parameters
+//   VariantSet<ListManager<SourceTypes,25>...> lists; // exactly one list manager per value type
+//   std::vector<std::shared_ptr<AnalysisDelegate>> notifiables; // more than one delegate per value type
+//   //std::vector< // runners
 
-  // template <typename FirstT>
-  // void registerRunner(FirstT first) {
-  //   first.setDestination(*this);
-  // }
+//   // template <typename FirstT>
+//   // void registerRunner(FirstT first) {
+//   //   first.setDestination(*this);
+//   // }
 
-  // template <typename FirstT, typename SecondT, typename... RestT>
-  // void registerRunner(FirstT first,SecondT second,RestT... rest) {
-  //   first.setDestination(*this);
-  //   registerRunner(second,rest...);
-  // }
-};
+//   // template <typename FirstT, typename SecondT, typename... RestT>
+//   // void registerRunner(FirstT first,SecondT second,RestT... rest) {
+//   //   first.setDestination(*this);
+//   //   registerRunner(second,rest...);
+//   // }
+// };
 
 }
 }
