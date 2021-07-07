aa38c16d6c98efbc1266ddf5f014a9ee6e73c317
Adam Fowler <adam@adamfowler.org>
2021-07-07 20:48:49 +0100

    Added matching key seed test.
    Signed-off-by: Adam Fowler <adam@adamfowler.org>

--- herald-tests/crossplatform-tests.cpp
+++ herald-tests/crossplatform-tests.cpp
@@ -37,3 +37,33 @@ TEST_CASE("crossplatform-contact-identifier", "[.][payload][crossplatform][basic
     testutil::validateEqual("contactIdentifier.csv");
   }
 }
+
+
+TEST_CASE("crossplatform-k-matchingkeyseed", "[.][payload][k-matchingkeyseed][basic]") {
+  SECTION("crossplatform-k-matchingkeyseed") {
+    // Check output file can be created
+    auto fn = testutil::fullFilename("kMatchingSeed.csv");
+    INFO("Output filename: " << fn);
+    std::ofstream cppOut(fn);
+    cppOut << "day,matchingSeed" << std::endl;
+
+    herald::payload::simple::SecretKey ks1((std::byte)0, 2048);
+    herald::payload::simple::K myK(2048, 2000, 240); // have to generate 2000 to get the right end key value
+    auto last(herald::payload::simple::F::h(ks1));
+    herald::payload::simple::MatchingKey nks;
+    // Note the indexes below are the value of i. I.e. today is day 0, tomorrow is day 1, etc.
+    // So we start with day 2001 (at index 2000)
+    cppOut << 2000 << "," << herald::datatype::Base64String::encode(last).encoded() << std::endl;
+    for (int day = 1999; day >= 0; --day) {
+      nks = herald::payload::simple::F::h(herald::payload::simple::F::t(last));
+      auto mks = herald::datatype::Base64String::encode(nks).encoded();
+      cppOut << day << ","
+             << mks << std::endl;
+      last = nks;
+    }
+    cppOut.close(); // flushes and closes
+
+    // Now ensure our output matches the other platforms'
+    testutil::validateEqual("kMatchingSeed.csv");
+  }
+}
